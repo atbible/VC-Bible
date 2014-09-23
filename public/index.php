66a51a323330414e8a28a42c8bbf07cee416d8e1
Andy Truong <thehongtt@gmail.com>
2014-09-24 00:21:12 +0700

    Fix wrong application using.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-use AndyTruong\PPP\Web\Application;
+use AndyTruong\Bible\Application;
 
 @include_once dirname(__DIR__) . '/vendor/autoload.php';
 @include_once dirname(__DIR__) . '/../../autoload.php';
 
-$app = new Application();
+$app = new Application;
 if (isset($_SERVER['REQUEST_URI'])) { // Handle default all web requests
     $app->getRestler()->handle();
 }
