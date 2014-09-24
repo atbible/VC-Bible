3a87644d19bfca06168bb964c8a79fe2e09e1a30
Andy Truong <thehongtt@gmail.com>
2014-09-25 01:43:28 +0700

    Make CLI runnable.

--- src/Command/ImportTKVNCommand.php
+++ src/Command/ImportTKVNCommand.php
@@ -2,8 +2,8 @@
 
 namespace AndyTruong\Bible\Command;
 
+use AndyTruong\App\Command;
 use AndyTruong\Bundle\ImportBundle\Helper\ImportHelper;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,8 +14,8 @@ class ImportTKVNCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('vcbible:import')
-            ->setDescription('Strart importing')
+            ->setName('bible:import')
+            ->setDescription('Strart importing…')
             ->addOption('restart', null, InputOption::VALUE_OPTIONAL, 'Restart importing process', false)
             ->addOption('limit', null, InputOption::VALUE_OPTIONAL, 'Restart importing process', 100)
         ;
@@ -27,15 +27,16 @@ class ImportTKVNCommand extends Command
         $manager = new ImportHelper($this->getContainer());
 
         if ($restart = (bool) $input->getOption('restart')) {
-            $manager->generateQueueItems();
+            return $manager->generateQueueItems();
         }
-        else {
-            for ($i = $limit; $i >= 0; $i--) {
-                if ($queue_item = $manager->getQueueItem()) {
-                    $output->writeln('Importing ' . $queue_item->getDescription(), OutputInterface::VERBOSITY_NORMAL);
-                    $manager->processQueueItem($queue_item);
-                }
+
+        for ($i = $limit; $i >= 0; $i--) {
+            if (!$queue_item = $manager->getQueueItem()) {
+                continue;
             }
+
+            $output->writeln('Importing ' . $queue_item->getDescription(), OutputInterface::VERBOSITY_NORMAL);
+            $manager->processQueueItem($queue_item);
         }
     }
 
